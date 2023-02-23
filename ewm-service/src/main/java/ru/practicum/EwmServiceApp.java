19fbbfcdd32fd246a5b0cec5c6ded8025fb722ba
NFilin <nickadm@yandex.ru>
2023-02-23 17:54:04 +0300

    добавлена заглушка для EwmServiceApp

---
+++ ewm-service/src/main/java/ru/practicum/EwmServiceApp.java
@@ -0,0 +1,11 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwmServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EwmServiceApp.class, args);
+    }
+}
