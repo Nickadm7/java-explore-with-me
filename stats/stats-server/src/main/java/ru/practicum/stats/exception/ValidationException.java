52c88c750aa1940c131249de1f5e954d03da4ba5
NFilin <nickadm@yandex.ru>
2023-02-23 18:01:49 +0300

    codestyle

--- stats/stats-server/src/main/java/ru/practicum/stats/exception/ValidationException.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/exception/ValidationException.java
@@ -1,6 +1,6 @@
 package ru.practicum.stats.exception;
 
-public class ValidationException extends RuntimeException{
+public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
     }
